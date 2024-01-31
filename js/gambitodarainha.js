dee270d06fe178e38c62bbeb95cb0276b3b30eeb
Ravagnanimarea <ravagnanim73@gmail.com>
2024-01-30 21:33:22 -0300

    Criação de um Projeto HTML, CSS e JavaScript

---
+++ js/gambitodarainha.js
@@ -0,0 +1,8 @@
+$('.gambito_beth').slick({
+    infinite: true,
+    speed: 200,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.slider-principal').slick({
+    infinite: true,
+    speed: 200,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
